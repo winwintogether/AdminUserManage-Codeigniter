fcef3bcecdfd517599df80f0fa9dac8f0d02c478
winwintogether <masterinbusiness@outlook.com>
2017-01-10 13:25:05 +0800

    Update code

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -69,16 +69,7 @@ class Login extends CI_Controller
             {
                 foreach ($result as $res)
                 {
-                    $sessionArray = array('userId'=>$res->userId,                    
-                                            'role'=>$res->roleId,
-                                            'roleText'=>$res->role,
-                                            'name'=>$res->name,
-                                            'isLoggedIn' => TRUE
-                                    );
-                                    
-                    $this->session->set_userdata($sessionArray);
-                    
-                    redirect('/dashboard');
+                   
                 }
             }
             else
