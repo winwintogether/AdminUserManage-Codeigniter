93c1fca466868aef7b70ca17299c151988ffe626
winwintogether <masterinbusiness@outlook.com>
2017-01-16 13:26:40 +0800

    Update code

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -51,13 +51,7 @@ class Login extends CI_Controller
     {
         $this->load->library('form_validation');
         
-        $this->form_validation->set_rules('email', 'Email', 'required|valid_email|max_length[128]|xss_clean|trim');
-        $this->form_validation->set_rules('password', 'Password', 'required|max_length[32]|');
-        
-        if($this->form_validation->run() == FALSE)
-        {
-            $this->index();
-        }
+      
         else
         {
             $email = $this->input->post('email');
